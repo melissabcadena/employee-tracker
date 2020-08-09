3552d54e64b7cc18aac2f2af2f74cc88bd7e6480
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-08-09 11:32:37 -0500

    add schema for db and tables

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
+
+CREATE TABLE departments(
+    deptName VARCHAR(30) NOT NULL,
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    jobTitle VARCHAR(30) NOT NULL,
+    salary INTEGER(11) NOT NULL,
+    deptID INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (deptID) REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    PRIMARY KEY (id),
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    salary INTEGER(11),
+    managerID INTEGER(11) NOT NULL,
+    FOREIGN KEY (managerID) REFERENCES employees(id),
+    deptID INTEGER(11) NOT NULL, 
+    FOREIGN KEY (deptID) REFERENCES departments(id),
+    roleID INTEGER(11) NOT NULL,
+    FOREIGN KEY (roleID) REFERENCES roles(id)
+);
